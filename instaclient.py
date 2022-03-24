ec0b6318703103e91d0dbaae7c420c86301f3d83
mr Shobadehgar <101979989+shobadeh@users.noreply.github.com>
2022-03-24 17:01:31 +0430

    Update instaclient.py

--- instaclient.py
+++ instaclient.py
@@ -1,8 +1,8 @@
 from requests import Session
 from random import choice
 
-from libs.utils import CheckPublicIP, IsProxyWorking
-from libs.utils import PrintStatus, PrintSuccess, PrintError
+from utils import CheckPublicIP, IsProxyWorking
+from utils import PrintStatus, PrintSuccess, PrintError
 
 USER_AGENTS = ["Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0",
 "Mozilla/5.0 (Android 4.4; Tablet; rv:41.0) Gecko/41.0 Firefox/41.0",
